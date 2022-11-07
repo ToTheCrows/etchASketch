1589b36af874989560408eec58e31ce9a8667d3c
Raphael Tango05 <raphael.tango05@gmail.com>
2022-11-07 21:43:23 +0100

    get full DOM into js

---
+++ script.js
@@ -0,0 +1,27 @@
+//Get 2 variables for color and size to change and use in functions
+const currentColor="black";
+const currentSize=16;
+
+//Get parent for Sketch creation
+const grid = document.querySelector(".grid");
+
+// Get Variables to hold the Color-Buttons
+const white = document.querySelector(".white");
+const black = document.querySelector(".black");
+const red = document.querySelector(".red");
+const blue = document.querySelector(".blue");
+const yellow = document.querySelector(".yellow");
+const green = document.querySelector(".green");
+const violet = document.querySelector(".violet");
+const pink = document.querySelector(".pink");
+
+//Get Erase-Button
+const erase = document.querySelector(".erase");
+
+//Get size-value
+const newSize = document.querySelector(".newSize");
+
+//Get changeSize-Button
+const changeSize = document.querySelector(".changeSize");
+
+
